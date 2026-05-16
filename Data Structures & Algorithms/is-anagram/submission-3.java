577daf8950464882fdaa63c33a430a8902be24d3
Furqan Moin <furqan.moin7@gmail.com>
2026-05-16 21:25:34 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char ch1 []= s.toCharArray();
+        char ch2 [] = t.toCharArray();
+            Arrays.sort(ch1);
+            Arrays.sort(ch2);
+            int a= ch1.length; int b = ch2.length;
+            if(a!=b) return false;
+                for(int i = 0; i<a; i++){
+                    if(ch1[i]!=ch2[i])
+                    return false;
+                
+            }
+    return true;
+    }
+}
